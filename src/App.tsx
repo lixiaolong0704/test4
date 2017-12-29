d07be32e8d388105cdb0e1c1f69d39d48bb1fa4f
leibo.sun@ptmind.com <leibo.sun@ptmind.com>
2017-12-29 17:16:56 +0800

    test

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import './App.css';
+
+
+import { observable, computed } from "mobx";
+
+class OrderLine {
+    @observable price = 0;
+    @observable amount = 1;
+
+    @computed get total() {
+        return this.price * this.amount;
+    }
+}
+
+var a =new OrderLine();
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
