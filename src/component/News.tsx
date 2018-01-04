9e387577dde73ab45db929a087b493cbce062302
leibo.sun@ptmind.com <leibo.sun@ptmind.com>
2018-01-04 19:55:06 +0800

    test

--- src/component/News.tsx
+++ src/component/News.tsx
@@ -7,24 +7,44 @@ const Option = Select.Option;
 const AutoCompleteOption = AutoComplete.Option;
 import {observable, computed} from 'mobx';
 import {observer} from "mobx-react";
-
+import elementClass from 'element-class';
 @observer
 export default class News extends React.Component {
 
     @observable se: any;
 
+
+
+    start_el:any;
+
     onMouseDown(e) {
         // console.log(e);
-        if (e.target.className === 'canvas-reader__el') {
+        if (elementClass(e.target).has('canvas-reader__el')) {
             this.se = e.target.innerText;
+            // e.target.className.
+            elementClass(e.target).add('canvas-reader__el_selected')
+            this.start_el = e.target;
         }
     }
+    onMouseMove(e){
+        if (elementClass(e.target).has('canvas-reader__el')) {
 
+            var end_el = e.target;
+
+
+
+        }
+
+
+    }
     onMouseUp() {
 
+        this.start_el =null;
     }
 
     render() {
+
+
 
 
         var str = '';
@@ -32,7 +52,7 @@ export default class News extends React.Component {
 
             var aaa = `As your app grows, you can catch a lot of bugs with typechecking. For some applications, you can use JavaScript extensions like Flow or TypeScript to typecheck your whole application. But even if you don’t use those, React has some built-in typechecking abilities. To run typechecking on the props for a component, you can assign the special propTypes property:`;
             var rpValue = `<span class="canvas-reader__el">$1</span>`;
-            str += "<p>"+aaa.replace(/([a-zA-Z]*)/g, rpValue)+"</p>";
+            str += "<p>"+aaa.replace(/([a-zA-Z’-]+|[\,\.,:])/g, rpValue)+"</p>";
 
         }
 
@@ -42,6 +62,7 @@ export default class News extends React.Component {
                 {this.se}
                 <div onMouseDown={this.onMouseDown.bind(this)}
                      onMouseUp={this.onMouseUp.bind(this)}
+                     onMouseMove={this.onMouseMove.bind(this)}
                      className='canvas-reader'
                      dangerouslySetInnerHTML={{__html: str}}>
 
