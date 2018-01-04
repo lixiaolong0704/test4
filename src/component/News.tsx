39d82b580ef495dbf5d08e3c5088452697b0a5e7
leibo.sun@ptmind.com <leibo.sun@ptmind.com>
2018-01-04 16:40:44 +0800

    ttt

--- src/component/News.tsx
+++ src/component/News.tsx
@@ -7,27 +7,34 @@ const Option = Select.Option;
 const AutoCompleteOption = AutoComplete.Option;
 import {observable, computed} from 'mobx';
 import {observer} from "mobx-react";
+
 @observer
 export default class News extends React.Component {
 
-    @observable   se:any;
+    @observable se: any;
+
     onMouseDown(e) {
         // console.log(e);
-        if(e.target.className==='canvas-reader__el'){
+        if (e.target.className === 'canvas-reader__el') {
             this.se = e.target.innerText;
         }
     }
-    onMouseUp(){
+
+    onMouseUp() {
 
     }
 
     render() {
 
 
-        var str = `As your app grows, you can catch a lot of bugs with typechecking. For some applications, you can use JavaScript extensions like Flow or TypeScript to typecheck your whole application. But even if you don’t use those, React has some built-in typechecking abilities. To run typechecking on the props for a component, you can assign the special propTypes property:`;
+        var str = '';
+        for (var i = 0; i < 100; i++) {
 
-        var rpValue = `<span class="canvas-reader__el">$&</span>`;
-        str = str.replace(/[a-zA-Z]*/g, rpValue);
+            var aaa = `As your app grows, you can catch a lot of bugs with typechecking. For some applications, you can use JavaScript extensions like Flow or TypeScript to typecheck your whole application. But even if you don’t use those, React has some built-in typechecking abilities. To run typechecking on the props for a component, you can assign the special propTypes property:`;
+            var rpValue = `<span class="canvas-reader__el">$1</span>`;
+            str += "<p>"+aaa.replace(/([a-zA-Z]*)/g, rpValue)+"</p>";
+
+        }
 
 
         return (
